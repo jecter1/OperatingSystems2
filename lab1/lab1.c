eb79f7ab1cc229854350ca59da5955962156ec6f
jecter <jecter1995@gmail.com>
2020-10-23 17:37:17 +0700

    lab1 & lab2

---
+++ lab1/lab1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void* printLines(void* str) {
+	for (int i = 1; i <= 10; ++i) {
+		printf("[Thread \"%s\"] Line #%d\n", (char*)str, i);
+	}
+
+	return (void*)EXIT_SUCCESS;
+}
+
+int main() {
+	char* firstThreadName = "1st";
+	char* secondThreadName = "2nd";
+	pthread_t secondThread;
+
+	if (0 != pthread_create(&secondThread, NULL, &printLines, (void*)secondThreadName)) {
+		perror("Couldn't create second thread");
+		pthread_exit((void*)EXIT_FAILURE);
+	}
+
+	printLines((void*)firstThreadName);
+	pthread_exit((void*) EXIT_SUCCESS);
+}
